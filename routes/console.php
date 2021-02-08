a7c817985afdc85c66658fd81e40d001ef2be862
Juan Carlos Orrego <jcorrego@gmail.com>
2021-02-08 18:04:49 -0500

    Duplicate users in course

--- routes/console.php
+++ routes/console.php
@@ -9,16 +9,6 @@ use GuzzleHttp\Client;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Mail;
 
-/*
-|--------------------------------------------------------------------------
-| Console Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of your Closure based console
-| commands. Each Closure is bound to a command instance allowing a
-| simple approach to interacting with each command's IO methods.
-|
-*/
 Artisan::command('people:sync {limit?} {offset?}', function ($limit = 25, $offset = 0) {
     $client = new Client();
     $count  = 0;
@@ -94,12 +84,12 @@ Artisan::command('people:registered-mail', function () {
             $course = $member->courses()->where('period', $period)->first();
             $this->line($member->name . ': ' . $course->name);
             Mail::to($member->email)->send(new CourseRegisteredMail($course, $member));
-//            Mail::to('jcorrego@gmail.com')->send(new CoursePreRegisteredMail($course, $member));
         } else {
             $this->error($member->name . ': No Email');
         }
     }
 })->describe('Send emails to registered students');
+
 Artisan::command('courses:professor-mail', function () {
     $period  = '2021-1';
     $courses = Course::where('period', $period)->has('members')->get();
@@ -108,7 +98,6 @@ Artisan::command('courses:professor-mail', function () {
             if ($prof->email && $prof->id == 35918102) {
                 $this->line($prof->id . '. ' . $prof->name . ': ' . $course->name);
                 Mail::to($prof->email)->send(new CourseSummaryMail($course, $prof));
-//            Mail::to('jcorrego@gmail.com')->send(new CoursePreRegisteredMail($course, $member));
             } else {
                 $this->error($prof->id . '. ' . $prof->name . ': No Email');
             }
@@ -117,6 +106,7 @@ Artisan::command('courses:professor-mail', function () {
         }
     }
 })->describe('Send emails to professor with course summary');
+
 Artisan::command('courses:duplicate {course_id?}', function ($course_id = 0) {
     if($course_id){
         $period = '2021-1';
@@ -126,9 +116,9 @@ Artisan::command('courses:duplicate {course_id?}', function ($course_id = 0) {
         $new->save();
         $new->professor()->associate($original->professor);
         $new->ministry()->associate($original->ministry);
-//        foreach ($original->members as $member){
-//            $new->members()->attach($member->id);
-//        }
+       foreach ($original->members as $member){
+           $new->members()->attach($member->id);
+       }
         $new->save();
     }
 })->describe('Duplicates a course');
